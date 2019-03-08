db5390b1992fa623e8877eed3dc1dc51815000d4
UnknownMonk <smodcartoon@gmail.com>
2019-03-08 13:00:06 -0500

    Added called action in app.js with component did mount and brought in actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -6,7 +6,12 @@ import axios from 'axios';
 export const GETLOADING = 'GETLOADING';
 export const GETSUCCESS = 'GETSUCCESS';
 export const GETFAILURE = 'GETFAILURE';
-
+export const ADDLOADING = 'ADDLOADING';
+export const ADDSUCCESS = 'ADDSUCCESS';
+export const ADDFAILURE = 'ADDFAILURE';
+export const DELLOADING = 'DELLOADING';
+export const DELSUCCESS = 'DELSUCCESS';
+export const DELFAILURE = 'DELFAILURE';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -34,6 +39,46 @@ export const fetchSmurfs = () => {
       })
       .catch(err => {
         dispatch({ type: GETFAILURE, payload: 'No Smurf for you' });
+      });
+  };
+};
+
+export const addSmurfs = smurf => {
+  return dispatch => {
+    dispatch({ type: ADDLOADING });
+    axios
+      .post(`http://localhost:3333/smurfs`, smurf)
+
+      .then(res => {
+        console.log(res);
+
+        dispatch({
+          type: ADDSUCCESS,
+          payload: res.data
+        });
+      })
+      .catch(err => {
+        dispatch({ type: ADDFAILURE, payload: 'No Smurf for you' });
+      });
+  };
+};
+
+export const delSmurf = id => {
+  return dispatch => {
+    dispatch({ type: DELLOADING });
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+
+      .then(res => {
+        console.log(res);
+
+        dispatch({
+          type: DELSUCCESS,
+          payload: res.data
+        });
+      })
+      .catch(err => {
+        dispatch({ type: DELFAILURE, payload: 'No Smurf for you' });
       });
   };
 };
